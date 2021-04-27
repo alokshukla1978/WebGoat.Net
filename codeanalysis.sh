2e80d4d3f02c8e840111a7a78b86cfc0d70696a0
Alok Shukla <32108973+alokshukla1978@users.noreply.github.com>
2021-04-26 21:11:35 -0700

    Update codeanalysis.sh

--- codeanalysis.sh
+++ codeanalysis.sh
@@ -4,5 +4,4 @@ curl -O https://www.shiftleft.io/download/sl-latest-windows-x64.zip
 echo "Extracting Shiftleft Agent!"
 powershell -Command "& {Expand-Archive sl-latest-windows-x64.zip -DestinationPath . ;}"
 echo "Perform Code Analysis!"
-$BUILD_SOURCESDIRECTORY\\sl analyze --app WebGoatNET-$BUILD_BUILDID --wait --cpg --csharp --dotnet-framework --csharp2cpg-args "-l info" "$BUILD_SOURCESDIRECTORY\\WebSite\WebSite.csproj"
-sleep 10
+$BUILD_SOURCESDIRECTORY\\sl analyze --app WebGoatNET --wait --cpg --csharp --dotnet-framework --csharp2cpg-args "-l info" "$BUILD_SOURCESDIRECTORY\\WebSite\WebSite.csproj"
